08c20991fc7c2a750969bdfe7efab53524fed51f
Brian <salas.pardo13@gmail.com>
2024-01-12 14:35:06 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Servidor escuchando en el puerto 5000');
+});
