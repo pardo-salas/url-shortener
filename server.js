391f98044f836697fd37535a39c447bfd725a36e
Brian <salas.pardo13@gmail.com>
2024-01-24 20:55:33 -0600

    añadí función de devolver el url recortado

--- server.js
+++ server.js
@@ -1,33 +1,52 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const ShortUrl = require('./models/shortUrl')
+const shortUrl = require('./models/shortUrl');
+const createHttpError = require('http-errors')
 const app = express();
 
 mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
 })
+.then(()=>console.log('mongoose connected'))
+.catch((error)=>console.log('Error Connecting...'))
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async(req, res) => {
-  const shortUrls = await ShortUrl.find()
+  const shortUrls = await shortUrl.find()
   res.render('index', { shortUrls: shortUrls });
 });
 
-app.post('/shortUrl',async(req,res)=>{
-  await ShortUrl.create({ full: req.body.fullUrl })
-  res.redirect('/')
+app.post('/shortUrl',async(req,res,next)=>{
+  try{
+    const {fullUrl} = req.body
+    
+    if(!fullUrl){
+      throw createHttpError.BadRequest('Provide a valid url')
+    }
+    const urlExist = await shortUrl.findOne({ full: fullUrl });
+    if(urlExist){
+      res.render('index',{short_url: `http://localhost:5000/${urlExist.short}`})
+      return
+    }
+    const response_url= await shortUrl.create({ full: fullUrl })
+    const result = await response_url.save()
+    
+    res.render('index',{short_url: `http://localhost:5000/${result.short}`})
+  }catch (error){
+    next(error)
+  }
 })
 
 app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+  const url = await shortUrl.findOne({ short: req.params.shortUrl })
+  if (url == null) return res.sendStatus(404)
 
-  shortUrl.clicks++
-  shortUrl.save()
+  url.clicks++
+  url.save()
 
-  res.redirect(shortUrl.full)
+  res.redirect(url.full)
 })
 
 app.listen(process.env.PORT || 5000, () => {
